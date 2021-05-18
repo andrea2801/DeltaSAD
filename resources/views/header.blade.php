9950ed86870a19549f472ded98a0ca519e5c852c
linux <linux@debian10.4>
2021-05-18 20:22:11 +0200

    Feat(inicio) header

---
+++ resources/views/header.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+<div class="contendor">
+    <div class="row justify-content-center">
+            <div class="col-md-1">
+             <img class="col logo" src="{{asset('img/Logo2.png')}}">
+            </div>
+            <div class="col">
+                    <h2>Bienvenido/a Usuario</h2>
+            </div>
+            <div class="col">
+                    <h2>15/02/2020</h2>
+            </div>
+            <div class="col">
+                    <h2>Salir</h2>
+            </div>
+            <div class="col-md-1">
+            <img class="logo col-md-8" src="{{asset('img/logout.png')}}">
+ 
+            </div>
+</div>
+</div>
+
+
+
+</body>
+</html>
