f20ed62504b765edd0322565a0e5cbe72f9a70bb
Raquel Gutierrez <raquel.guan.dev@gmail.com>
2021-05-18 20:23:03 +0200

    Feat(usuarios): Insercion de la vista usuarios

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,35 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="container-fluid">
+<div class="container usuarios">
         <div class="row">
-            <div class="col-12">
-                <div class="row">
-                    <div class="col-12"></div>
-                    <div class="col-12"></div>
-                    <div class="col-12"></div>
-                </div>
+            <div class="col-12 mt-5 ml-5">
+                <h1 class="title-user">Usuarios</h1>
+            </div>
+            <div class="col-12 ml-5">
+                <h2 class="subtitle-user">Usuarios zona $x</h2>
+                <hr class="user-underline">
+            </div>
+
+            <div class="col-12 mt-3 ml-5">
+                <ul>
+                    <li class="user-list">$nombreabuelete</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6 ml-5">
+                <p>Esto será la paginacion</p>
             </div>
         </div>
     </div>
 
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
---
+++ resources/views/usuarios.blade.php
@@ -0,0 +1,22 @@
+<div class="container-fluid">
+@include()
+    <div class="row">
+        <div class="col-12">
+            <h1>Usuarios</h1>
+        </div>
+        <div class="col-12">
+            <h2>Usuarios zona $x</h2>
+        </div>
+        <hr>
+        <div class="col-12">
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-6">
+            <p>Esto será la paginacion</p>
+        </div>
+    </div>
+</div>
