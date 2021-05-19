830f16b49902bda2d0222ee540ade23994452075
Sandra González <sandra1398DAW@gmail.com>
2021-05-19 16:11:14 +0200

    Feat(home): Principio de la home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-5">
+            <p>NOTIFICACIONES RECIBIDAS</p>
+        </div>
+        <div class="col-3">
+            <button></button>
+        </div>
+        <div class="col-3">
 
+        </div>
+    </div>
+</div>
 
 @endsection
