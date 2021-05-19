bc6e3c973c687fc7184e83c99ba1bb2a740443df
Sandra González <sandra1398DAW@gmail.com>
2021-05-19 15:49:06 +0200

    Feat(home): Directorios css y blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container usuarios">
-        <div class="row">
-            <div class="col-12 mt-5 ml-5">
-                <h1 class="title-user">Usuarios</h1>
-            </div>
-            <div class="col-12 ml-5">
-                <h2 class="subtitle-user">Usuarios zona $x</h2>
-                <hr class="user-underline">
-            </div>
 
-            <div class="col-12 mt-3 ml-5">
-                <ul>
-                    <li class="user-list">$nombreabuelete</li>
-                </ul>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-6 ml-5">
-                <p>Esto será la paginacion</p>
-            </div>
-        </div>
-    </div>
 
 @endsection
