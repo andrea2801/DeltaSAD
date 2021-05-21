a59b884ec447278d182b925536c50b8b5ffb1105
linux <linux@debian10.4>
2021-05-21 15:42:47 +0200

    Feat(view trabajadora en proceso)

--- resources/views/front/trabajadora.blade.php
+++ resources/views/front/trabajadora.blade.php
@@ -1,177 +1,116 @@
 @extends('layouts.master')
 
 @section('content')
-
-<style>
-.container{
-    width:65%;
-    
-}
+<!--<style>.container{ width:70%;}-->
 
 </style>
-
 <div class="container-fluid p-0 m-0 d-flex usuarios">
-    @include('front.nav')
-    <div class="row">
-         <div class="col-12 mt-5 ml-5">
+   
+        @include('front.nav')       
+  <div class="row container-fluid justify-content-center aligin-items-center">
+         
+          <div class="col-12 mt-5 ml-5">
             <h1 class="title-user">Trabajadora</h1>
+          </div>
+          
+          <div class="col ml-5">  
+                   
+            <h2 class="subtitle-user">Tu equipo</h2> 
+     </div>
+          <div class="col">
+            <ul class="nav justify-content-end">
+                  <li class="nav-items">
+                    <a class="nav-link active" href="#"><h2>Crear</h2></a>
+                  </li>
+                  <li class="nav-items border-left">
+                    <a class="nav-link active" href="#"><h2>Mostrar</h2></a>
+                  </li>
+                  
+             </ul>             
+      
+   
+           </div>           
+
+<div class="col-12 mt-3 ml-5">
+  <hr class="user-underline">
+  <div class="container justify-content-center align-items-center vh-100">
+      <!--grupo1-->
+      <div class="card-deck">
+
+      <div class="card text-center" style="width: 18rem;">
+                <img src="/img/icons/trabajadora.png" class="card-img-top" alt="...">
+                <div class="card-body bg-primary">
+                  <h5 class="card-title">Maria Dolors</h5>   
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">933 444 222</li>
+                  <li class="list-group-item">maria@gmail.com</li>
+                  <li class="list-group-item">Zona I</li>
+                </ul>
+                <div>
+                  <a href="#" class="card-link">Horarios</a>
+                  <a href="#" class="card-link">Usuarios</a>
+                </div>
         </div>
-        <div class="col-12  ml-5">
-            <h2 class="subtitle-user">Tu equipo</h2>
-            <hr class="user-underline">
+        
+
+
+       <div class="card text-center" style="width: 18rem;">
+                <img src="/img/icons/trabajadora.png" class="card-img-top" alt="...">
+                <div class="card-body bg-primary">
+                  <h5 class="card-title">Maria Dolors</h5>   
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">933 444 222</li>
+                  <li class="list-group-item">maria@gmail.com</li>
+                  <li class="list-group-item">Zona I</li>
+                </ul>
+                <div>
+                  <a href="#" class="card-link">Horarios</a>
+                  <a href="#" class="card-link">Usuarios</a>
+                </div>
         </div>
 
-        <div class="col-12 mt-3 ml-5">
-
-
-<div class="container justify-content-center align-items-center vh-100">
-
-<div class="card-deck" >
-
-<div class="card text-center" style="width: 10rem;">
-  <img src="/img/icons/trabajadora.png" class="card-img-top" alt="...">
-  <div class="card-body bg-primary">
-    <h5 class="card-title">Maria Dolors</h5>   
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">933 444 222</li>
-    <li class="list-group-item">ma@gmail.com</li>
-    <li class="list-group-item">Zona I</li>
-  </ul>
-  <div>
-    <a href="#" class="card-link">Horarios</a>
-    <a href="#" class="card-link">Usuarios</a>
-  </div>
-</div>
-
-<!--car2 2 -->
-<div class="card text-center" style="width: 10rem;">
-  <img src="/img/icons/trabajadora.png" class="card-img-top" alt="...">
-  <div class="card-body bg-primary">
-    <h5 class="card-title">Maria Dolors</h5>   
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">933 444 222</li>
-    <li class="list-group-item">maria@gmail.com</li>
-    <li class="list-group-item">Zona I</li>
-  </ul>
-  <div>
-    <a href="#" class="card-link">Horarios</a>
-    <a href="#" class="card-link">Usuarios</a>
-  </div>
-</div>
-<!--card3-->
-<div class="card text-center" style="width: 10rem;">
-  <img src="/img/icons/trabajadora.png" class="card-img-top" alt="...">
-  <div class="card-body bg-primary">
-    <h5 class="card-title">Maria Dolors</h5>   
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">933 444 222</li>
-    <li class="list-group-item">maria@gmail.com</li>
-    <li class="list-group-item">Zona I</li>
-  </ul>
-  <div>
-    <a href="#" class="card-link">Horarios</a>
-    <a href="#" class="card-link">Usuarios</a>
-  </div>
-</div>
-<!--card4-->
-
-<div class="card text-center" style="width: 10rem;">
-  <img src="/img/icons/trabajadora.png" class="card-img-top" alt="...">
-  <div class="card-body bg-primary">
-    <h5 class="card-title">Maria Dolors</h5>   
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">933 444 222</li>
-    <li class="list-group-item">maria@gmail.com</li>
-    <li class="list-group-item">Zona I</li>
-  </ul>
-  <div>
-    <a href="#" class="card-link">Horarios</a>
-    <a href="#" class="card-link">Usuarios</a>
-  </div>
-</div>
-
-
-</div>
-
-<div class="row card-deck mt-5">
-
-<div class="card text-center" style="width: 10rem;">
-  <img src="/img/icons/trabajadora.png" class="card-img-top" alt="...">
-  <div class="card-body bg-primary">
-    <h5 class="card-title">Maria Dolors</h5>   
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">933 444 222</li>
-    <li class="list-group-item">maria@gmail.com</li>
-    <li class="list-group-item">Zona I</li>
-  </ul>
-  <div>
-    <a href="#" class="card-link">Horarios</a>
-    <a href="#" class="card-link">Usuarios</a>
-  </div>
-  </div>
-
-
-<!--car2 2 -->
-<div class="card text-center" style="width: 10rem;">
-  <img src="/img/icons/trabajadora.png" class="card-img-top" alt="...">
-  <div class="card-body bg-primary">
-    <h5 class="card-title">Maria Dolors</h5>   
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">933 444 222</li>
-    <li class="list-group-item">maria@gmail.com</li>
-    <li class="list-group-item">Zona I</li>
-  </ul>
-  <div>
-    <a href="#" class="card-link">Horarios</a>
-    <a href="#" class="card-link">Usuarios</a>
-  </div>
-</div>
-<!--card3-->
-<div class="card text-center" style="width: 10rem;">
-  <img src="/img/icons/trabajadora.png" class="card-img-top" alt="...">
-  <div class="card-body bg-primary">
-    <h5 class="card-title">Maria Dolors</h5>   
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">933 444 222</li>
-    <li class="list-group-item">maria@gmail.com</li>
-    <li class="list-group-item">Zona I</li>
-  </ul>
-  <div>
-    <a href="#" class="card-link">Horarios</a>
-    <a href="#" class="card-link">Usuarios</a>
-  </div>
-</div>
-<!--card4-->
-
-<div class="card text-center">
-  <img src="/img/icons/trabajadora.png" class="card-img-top" alt="...">
-  <div class="card-body bg-primary">
-    <h5 class="card-title">Maria Dolors</h5>   
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">933 444 222</li>
-    <li class="list-group-item">maria@gmail.com</li>
-    <li class="list-group-item">Zona I</li>
-  </ul>
-  <div>
-    <a href="#" class="card-link">Horarios</a>
-    <a href="#" class="card-link">Usuarios</a>
-  </div>
-</div>
-
-
-</div>
-
+        <div class="card text-center"style="width: 18rem;">
+                  <img src="/img/icons/trabajadora.png" class="card-img-top" alt="...">
+                  <div class="card-body bg-primary">
+                    <h5 class="card-title">Maria Dolors</h5>   
+                  </div>
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item">933 444 222</li>
+                    <li class="list-group-item">maria@gmail.com</li>
+                    <li class="list-group-item">Zona I</li>
+                  </ul>
+                  <div>
+                    <a href="#" class="card-link">Horarios</a>
+                    <a href="#" class="card-link">Usuarios</a>
+                  </div>
         </div>
+
+        <div class="card text-center"style="width: 18rem;">
+                  <img src="/img/icons/trabajadora.png" class="card-img-top" alt="...">
+                  <div class="card-body bg-primary">
+                    <h5 class="card-title">Maria Dolors</h5>   
+                  </div>
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item">933 444 222</li>
+                    <li class="list-group-item">maria@gmail.com</li>
+                    <li class="list-group-item">Zona I</li>
+                  </ul>
+                  <div>
+                    <a href="#" class="card-link">Horarios</a>
+                    <a href="#" class="card-link">Usuarios</a>
+                  </div>
+        </div>
+      </div> 
+<!--grupo2-->
+
+
+
     </div>
+
+
 </div>
-
-
+</div>    
+</div>
 @endsection
