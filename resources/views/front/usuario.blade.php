5a10c4f30a303f88fa1adc5bfe3a620d9e874c26
Sandra González <sandra1398DAW@gmail.com>
2021-05-24 11:44:00 +0200

    Feat(header): Nuevo header sin terminar

--- resources/views/front/usuario.blade.php
+++ resources/views/front/usuario.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
 <div class="container-fluid p-0 m-0 d-flex usuarios">
-    @include('front.nav')
     <div class="row">
         <div class="col-12 mt-5 ml-5">
             <h1 class="title-user">Usuarios</h1>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,27 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="container-fluid">
-    <div class="row">
-        @include('front.nav')
-        <div class="col-5">
-            <p>NOTIFICACIONES RECIBIDAS</p>
-        </div>
-        <div class="col-3">
-            <button></button>
-        </div>
-        <div class="col-3">
-
+<div class="home">
+    <div class="container-fluid">
+        <div class="row">
+            @include('front.nav')
+            <div class="col-12">
+                <div class="row d-flex align-items-center justify-content-center">
+                    <div class="col-5">
+                        <p>NOTIFICACIONES RECIBIDAS</p>
+                    </div>
+                    <div class="col-3">
+                        <a href="">
+                            <button class="btn btn-primary">Ver enviadas</button>
+                        </a>
+                    </div>
+                    <div class="col-3">
+                        <a href="">
+                            <button class="btn btn-primary">Crear nueva</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
