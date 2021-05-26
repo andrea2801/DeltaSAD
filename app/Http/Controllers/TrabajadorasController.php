968e1d96cf575aaf1c2936bb31fe24b90ff885e7
nathalie888 <nquiroz2000@gmail.com>
2021-05-26 17:41:49 +0200

    Feat(crear-route-mostrarTrabajadoras)

--- app/Http/Controllers/TrabajadorasController.php
+++ app/Http/Controllers/TrabajadorasController.php
@@ -7,31 +7,45 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\IncidenciasController;
 use App\Models\User;
+use App\Models\Flash;
 
 
 class TrabajadorasController extends Controller
 {
     public function index(Request $request){
-
-        return view('front/trabajadoras/index');
-
+        $tfs = $this->showTFS();
+        return view('front/trabajadoras/index')->with('tfs', $tfs);
     }
 
     public function store(Request $request){
-
         $inputs=$request->all();
         $inputs['password']=bcrypt($inputs['password']);
 
-       // dd($inputs);
-
-        //Flas::success('ok');
+       //dd($inputs);
+       //Flash::success('ok');
+       //@include('Flash::message') esto en index
         User::create($inputs);
-
 
     return redirect(route('trabajadoras.index'));
 
-
+    }
+    protected function showTFS(){
+       $tfs = DB::table('users')->where('rol_id', 2)->where('zona', Auth::user()->zona)->get();
+       return $tfs;
     }
 
+    protected function showTFusers($tfId){
+        $users=DB::table('usuarios')->where('tf_asignada', $tfId)->orWhere('tf_asignada2', $tfId)->get();
+        var_dump($users); die('holi');
+        return $users;
+    }
+
+
+
+    //mostrar todas
+
+    public function buscar(Request $request){
+        return view('front/trabajadoras/todas_trabajadoras');
+    }
 
 }
