c13235a23f349278930a0382a1d2d2a57199041f
Sandra González <sandra1398DAW@gmail.com>
2021-05-29 17:03:04 +0200

    Feat(leerNoti): Popup con responsive sin terminar

--- resources/views/front/notificaciones/popUpLeerNoti.blade.php
+++ resources/views/front/notificaciones/popUpLeerNoti.blade.php
@@ -4,60 +4,58 @@
             <div class="modal-body m-5">
                 <div class="row">
                     <div class="col-12">
-                        <div class="row d-flex">
-                            <div class="mb-5 col-12">
+                        <div class="row">
+                            <div class="mb-3 col-12">
                                 <div class="row">
-                                    <div class="col-2">
-                                        <p>De:</p>
+                                    <div class="col-12 justify-content-end d-flex mb-5">
+                                        <a href="#">
+                                            <img class="close-cross" src="{{asset('/img/icons/X.png')}}" alt="salir">
+                                        </a>
                                     </div>
-                                    <div class="col-4 border">
-                                        <p>Fulano</p>
+                                    <div class="col-12 justify-content-end d-flex mb-3">
+                                        <p class="first-text">Prioridad: <span class="content-text p-3">Alta</span></p>
                                     </div>
-                                    <div class="col-2">
-                                        <p>Para:</p>
+                                </div>
+                                <div class="row d-flex align-items-center">
+                                    <div class="col-12">
+                                        <p class="first-text">De:</p>
                                     </div>
-                                    <div class="col-4 border">
-                                        <p>Raquel</p>
+                                    <div class="col-12">
+                                        <p class="content-text p-3">Sandra González Marín</p>
+                                    </div>
+                                    <div class="col-12">
+                                        <p class="first-text">Para:</p>
+                                    </div>
+                                    <div class="col-12">
+                                        <p class="content-text p-3">Raquel Gutierrez Antequera</p>
                                     </div>
                                 </div>
                             </div>
-                            <div class="mb-5 col-12">
-                                <div class="row">
-                                    <div class="col-2">
-                                        <p>Estado:</p>
+                            <div class="mb-3 col-12">
+                                <div class="row d-flex align-items-center">
+                                    <div class="col-12 col-md-6 mb-4 mb-md-0">
+                                        <p class="first-text">Estado: <span class="content-text p-3">Nueva</span></p>
                                     </div>
-                                    <div class="col-2 border">
-                                        <p>Nueva</p>
-                                    </div>
-                                    <div class="col-2">
-                                        <p>Prioridad:</p>
-                                    </div>
-                                    <div class="col-2 border">
-                                        <p>Alta</p>
-                                    </div>
-                                    <div class="col-2">
-                                        <p>Fecha:</p>
-                                    </div>
-                                    <div class="col-2 border">
-                                        <p>12-12-12</p>
+                                    <div class="col-12 col-md-6">
+                                        <p class="first-text">Fecha: <span class="content-text p-3">29/05/21</span></p>
                                     </div>
                                 </div>
                             </div>
-                            <div class="mb-5 col-12">
-                                <div class="row">
-                                    <div class="col-2">
-                                        <p>Asunto:</p>
+                            <div class="mb-3 col-12">
+                                <div class="row d-flex align-items-center">
+                                    <div class="col-12">
+                                        <p class="first-text">Asunto:</p>
                                     </div>
-                                    <div class="col-10 border">
-                                        <p>Blablabla</p>
+                                    <div class="col-12">
+                                        <p class="content-text p-3">Blablabla</p>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-12">
-                                <p>Mensaje:</p>
+                                <p class="first-text">Mensaje:</p>
                             </div>
                             <div class="col-12">
-                                <textarea rows="10" cols="50">BlablablaBlablablaBlablablaBlablablaBlablablaBlablablaBlablablaBlablablaBlablablaBlablablaBlablablaBlablabla</textarea>
+                                <textarea class="content-text pl-3 pr-3 pb-3" rows="8" cols="60" readonly>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse maxime, facilis possimus tenetur libero natus earum sit quasi suscipit ipsam, sint, voluptatum cum laborum odit dolor debitis fuga voluptatibus facere! Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, aut iure sunt doloremque molestias rem aperiam animi facere possimus omnis est laborum perspiciatis, ad dicta voluptatem quae! Aut, cupiditate accusantium. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus assumenda officia dolores earum, odit placeat ratione in quam possimus tenetur, sit suscipit, eos pariatur quis ad ab obcaecati! Est, quae.</textarea>
                             </div>
                         </div>
                     </div>
