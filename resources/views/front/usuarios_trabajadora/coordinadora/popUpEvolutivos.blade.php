31d4933bdc296e1f9f66733e3777dbb0c2a15d1e
Sandra González <sandra1398DAW@gmail.com>
2021-06-01 18:51:26 +0200

    Fix(popEv): Cambiando clases de estilos

--- resources/views/front/usuarios_trabajadora/coordinadora/popUpEvolutivos.blade.php
+++ resources/views/front/usuarios_trabajadora/coordinadora/popUpEvolutivos.blade.php
@@ -1,15 +1,15 @@
 <div class="modal fade" id="evolutivos">
     <div class="modal-dialog">
-        <div class="modal-content">
+        <div class="modal-content content-box">
             <div class="modal-header">
-                <h4 >Nuevo evolutivo</h4>
+                <h4>Nuevo evolutivo</h4>
             </div>
             <div class="modal-body">
                 <form action="{{route('crear.evolutivo')}}" method="POST" class="form needs-validation" novalidate>
                     @foreach ($usuario as $u )
                     <div class="form-group row">
-                        <label for="evolucion" class="col-sm-12 col-form-label">Evolución de {{$u->nombre}} {{$u->apellidos}}:</label>
-                        <div class="col-sm-12">
+                        <label for="evolucion" class="col-12 col-form-label">Evolución de {{$u->nombre}} {{$u->apellidos}}:</label>
+                        <div class="col-12">
                             <textarea name="evolucion" type="textarea" class="form-control" required rows="4"></textarea>
                             <div class="invalid-feedback">
                                 Añadir evolutivo.
@@ -21,7 +21,7 @@
                         @endforeach
                     </div>
                     <div class="modal-footer">
-                        <button type="submit" class="btn btn-primary">Crear</button>
+                        <button type="submit" class="btn btn-general">Crear</button>
                     </div>
                     @endforeach
                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
